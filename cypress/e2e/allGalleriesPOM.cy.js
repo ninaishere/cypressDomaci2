b994673270631b32b6cf0278b701a6b9ca47e06c
ninaishere <raickovic2001@gmail.com>
2022-06-09 14:04:45 +0200

    all galleries

--- cypress/e2e/allGalleriesPOM.cy.js
+++ cypress/e2e/allGalleriesPOM.cy.js
@@ -1,1 +1,30 @@
 /// <reference types="Cypress" />
+
+import { loginPage } from "../page_objects/loginPage";
+import { createGalleryPage } from "../page_objects/createGalleryPage";
+
+let validEmail = "nina@gmail.com";
+let validPassword = "test123456";
+
+describe("all galleries page", () => {
+  before("login", () => {
+    cy.visit("/");
+    loginPage.clickLoginBtn();
+    cy.url().should("include", "/login");
+    loginPage.login(validEmail, validPassword);
+    cy.url().should("not.include", "/login");
+  });
+
+  it("all galleries with logged in user", () => {
+    cy.visit("/");
+    cy.url().should("not.include", "/login");
+    cy.get(createGalleryPage.createGalleryBtn);
+    cy.wait(3000);
+  });
+
+  it("all galleries with guest user", () => {
+    cy.visit("/");
+    cy.url().should("not.include", "/login");
+    cy.get(loginPage.loginBtn);
+  });
+});
